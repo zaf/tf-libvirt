165d7b274383704e62fdb667dcdacd0884ad5561
Lefteris Zafiris <zaf@fastmail.com>
2023-12-24 21:01:01 +0200

    Updated depedencies versions

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.3.5"
+  required_version = ">= 1.6.0"
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = ">= 0.7.1"
+      version = ">= 0.7.6"
     }
   }
 }
